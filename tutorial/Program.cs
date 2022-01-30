ca0b18cb819c8247a621a89d8f60dfd22ce268e8
User <User@DESKTOP-2V4NQ27>
2022-01-30 16:21:10 +0200

    Add project files.

---
+++ tutorial/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rod = 18;//ari9mos
+            string ass = "i play warcraft";//protash h le3h
+            bool tits = false;//true or false
+            char livrid = 'J';//grammata h xarakthras-symvolo
+
+            Console.WriteLine(rod);
+            Console.WriteLine(ass);
+            Console.WriteLine(tits);
+            Console.WriteLine(livrid);
+
+
+            string red = Console.ReadLine();
+            try
+            {
+                int redd = Convert.ToInt32(red);
+
+                if (rod > redd)
+                {
+                    Console.WriteLine("rod is bigger");
+                }
+                else
+                {
+                    Console.WriteLine("rod is smaller");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("i can't do that " + ex);
+            }
+            Console.ReadLine();
+            
+
+
+
+        }
+    }
+}
